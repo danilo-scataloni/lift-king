7759aeeb91fae36554a01e385cdb6cf5112975bc
Danilo Scataloni <danilomsca@gmail.com>
2026-01-07 22:37:05 -0300

    feat: add preview for LiftKingSimpleDialog to enhance UI testing

---
+++ app/src/main/java/com/daniloscataloni/liftking/previews/LiftKingSimpleDialogPreview.kt
@@ -0,0 +1,18 @@
+package com.daniloscataloni.liftking.previews
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.daniloscataloni.liftking.ui.components.LiftKingSimpleDialog
+
+@Preview
+@Composable
+fun LiftKingSimpleDialogPreview() {
+    LiftKingSimpleDialog(
+        title = "Título da Dialog!",
+        message = "Esta é uma mensagem de exemplo para a dialog do LiftKing.",
+        confirmButtonText = "Confirmar",
+        cancelButtonText = "Cancelar",
+        onDismiss = { },
+        onConfirm = { }
+    )
+}
